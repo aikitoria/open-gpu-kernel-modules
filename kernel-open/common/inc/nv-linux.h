52670f7fd6a7a214be6fad6d8d90854e8b7d8470
aikitoria <151776613+aikitoria@users.noreply.github.com>
2026-03-11 15:10:42 +0100

    Experimental: 5000x faster cudaHostRegister for 1G page backed memory, shrink device page tables

--- kernel-open/common/inc/nv-linux.h
+++ kernel-open/common/inc/nv-linux.h
@@ -916,6 +916,7 @@ typedef struct nv_alloc_s {
     unsigned int   cache_type;
     unsigned int   num_pages;
     unsigned int   order;
+    unsigned int   compound_order;      /* 0 = normal 4K pages, >0 = hugepage compound order */
     unsigned int   size;
     nvidia_pte_t  *page_table;          /* array of physical pages allocated */
     unsigned int   pid;
@@ -1041,6 +1042,7 @@ typedef struct nv_dma_map_s {
     NvU64 page_count;
     NvBool contiguous;
     NvU32 cache_type;
+    NvU64 page_granularity;             /* PAGE_SIZE for normal, hugepage_size for compound pages */
     struct sg_table *import_sgt;
 
     union
