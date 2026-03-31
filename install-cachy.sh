f421cdefcb8a380b4946fe30afdc4aa6649a2b25
avlin <avlin@hotmail.fr>
2026-03-31 21:07:14 +0200

    typo

--- install-cachy.sh
+++ install-cachy.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 export  IGNORE_CC_MISMATCH=1
-suddo rmmod nvidia_drm nvidia_modeset nvidia_uvm nvidia
+sudo rmmod nvidia_drm nvidia_modeset nvidia_uvm nvidia
 set -e
 make modules -j$(nproc) CC=clang LD=ld.lld AR=llvm-ar CXX=clang++ OBJCOPY=llvm-objcopy NV_VERBOSE=1
 sudo make modules_install -j$(nproc) CC=clang LD=ld.lld AR=llvm-ar CXX=clang++ OBJCOPY=llvm-objcopy NV_VERBOSE=1
